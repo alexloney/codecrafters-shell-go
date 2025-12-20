ff3dd8728f291d0437a6701063527442dd14772c
Alex <aloney.cpp@gmail.com>
2025-12-20 14:40:10 -0800

    Adding echo command output

--- app/main.go
+++ app/main.go
@@ -7,22 +7,53 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+// Display the shell prompt, maybe in the future this
+// could display user customizable prompts
+func displayPrompt() {
+	fmt.Print("$ ")
+}
+
+// Obtain the users input from stdin and trim any trailing newlines
+func fetchTrimmedInput() string {
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.TrimRight(command, "\r\n")
+}
+
+// First pass at tokenizing a string by splitting on spaces.
+// This is a naive implementation and does not handle quotes or escaped spaces.
+func tokenize(input string) []string {
+	return strings.Split(input, " ")
+}
+
+// Given a tokenized input, handle processing the command.
+// Returns false if the shell should exit, true otherwise.
+func handleInput(input []string) bool {
+	if len(input) == 0 {
+		return true
+	}
+
+	command := input[0]
+	args := input[1:]
+
+	if command == "exit" {
+		return false
+	} else if command == "echo" {
+		fmt.Println(strings.Join(args, " "))
+		return true
+	} else {
+		fmt.Printf("%s: command not found\n", command)
+	}
+
+	return true
+}
 
 func main() {
 	for true {
-		fmt.Print("$ ")
-
-		// Captures the user's command in the "command" variable
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-
-		trimmed_command := strings.TrimRight(command, "\r\n")
-		if trimmed_command == "exit" {
+		displayPrompt()
+		command := fetchTrimmedInput()
+		tokens := tokenize(command)
+		if !handleInput(tokens) {
 			break
 		}
-
-		fmt.Print(trimmed_command)
-		fmt.Println(": command not found")
 	}
 }
