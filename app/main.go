4e5efeb1de6c8dbba5299b6e9e0b6ae3b5c391b0
Alex <aloney.cpp@gmail.com>
2025-12-20 15:34:55 -0800

    Adding code to print executable path

--- app/main.go
+++ app/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -51,13 +54,69 @@ func (t Type) GetType() string {
 type Unknown struct {
 	ICommand
 	Name string
+	Args []string
 }
 
+func (u Unknown) isExecutable(path string) bool {
+	path, err := exec.LookPath(path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (u Unknown) fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	// Check if the error is specifically because the file does not exist
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	// For other errors (like permission issues), you might want to handle them differently or return false
+	return false
+}
+func (u Unknown) GetPath() string {
+	path_separator := string(os.PathListSeparator)
+	file_separator := string(os.PathSeparator)
+	path := os.Getenv("PATH")
+
+	directories := strings.Split(path, path_separator)
+	for _, dir := range directories {
+		full_path := dir + file_separator + u.Name
+		if u.fileExists(full_path) {
+			if u.isExecutable(full_path) {
+				return filepath.Clean(full_path)
+			}
+		}
+	}
+	return ""
+}
 func (u Unknown) Execute() {
+	path := u.GetPath()
+	if path != "" {
+		command := exec.Command(path, u.Args...)
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+		err := command.Start()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		command.Wait()
+		return
+	}
 	fmt.Printf("%s: command not found\n", u.Name)
 }
 func (u Unknown) GetType() string {
-	return fmt.Sprintf("%s: not found", u.Name)
+	path := u.GetPath()
+	if path != "" {
+		return fmt.Sprintf("%s is %s", u.Name, path)
+	} else {
+		return fmt.Sprintf("%s: not found", u.Name)
+	}
+
 }
 
 // Display the shell prompt, maybe in the future this
@@ -91,7 +150,7 @@ func createCommand(tokens []string) ICommand {
 	case "type":
 		return Type{Args: tokens[1:]}
 	default:
-		return Unknown{Name: tokens[0]}
+		return Unknown{Name: tokens[0], Args: tokens[1:]}
 	}
 }
 
